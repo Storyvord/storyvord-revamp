ebfc931c1aaa62f501653a79758c55ced71f43b6
rahberabbas <amaanabidi1234@gmail.com>
2024-07-03 00:43:09 +0530

    "Crew Profile and Crew other models added"

--- storyvord/crew/models.py
+++ storyvord/crew/models.py
@@ -1,22 +1,47 @@
-
-
-# Create your models here.
 from django.db import models
 from django.conf import settings
 
+# Create your models here.
 class CrewProfile(models.Model):
-    # USER_TYPE_CHOICES = [  # Change: Added USER_TYPE_CHOICES
-    #     ('client', 'Client'),
-    #     ('crew', 'Crew'),
-    #     ('vendor', 'Vendor'),
-    #     ('internal', 'Internal Team'),
-    # ]
-
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
-    image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
-    # user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)  # Change: Added user_type field
-
-    def __str__(self):
-        return f'{self.user.email}'
+    name = models.CharField(max_length=256, null=True, blank=True)
+    phone = models.CharField(max_length=256, null=True, blank=True)
+    location = models.CharField(max_length=256, null=True, blank=True)
+    languages = models.CharField(max_length=256, null=True, blank=True)
+    job_title = models.CharField(max_length=256, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    experience = models.CharField(max_length=256, null=True, blank=True)
+    skills = models.CharField(max_length=256, null=True, blank=True)
+    technicalProficiencies = models.CharField(max_length=256, null=True, blank=True)
+    specializations = models.CharField(max_length=256, null=True, blank=True)
+    drive = models.BooleanField(default=False)
+    
+class CrewCredits(models.Model):
+    crew = models.ForeignKey(CrewProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256, null=True, blank=True)
+    year = models.CharField(max_length=256, null=True, blank=True)
+    role = models.CharField(max_length=256, null=True, blank=True)
+    production = models.CharField(max_length=256, null=True, blank=True)
+    client = models.CharField(max_length=256, null=True, blank=True)
+    type_of_content = models.CharField(max_length=256, null=True, blank=True)
+    tags = models.CharField(max_length=256, null=True, blank=True)
+    
+class CrewEducation(models.Model):
+    crew = models.ForeignKey(CrewProfile, on_delete=models.CASCADE)
+    academicQualifications = models.CharField(max_length=256, null=True, blank=True)
+    professionalCourses = models.CharField(max_length=256, null=True, blank=True)
+    workshopsAttended = models.CharField(max_length=256, null=True, blank=True)
+    
+class CrewRate(models.Model):
+    crew = models.ForeignKey(CrewProfile, on_delete=models.CASCADE)
+    standardRate = models.CharField(max_length=256, null=True, blank=True)
+    negotiation = models.BooleanField(default=False)
+    
+class EndorsementfromPeers(models.Model):
+    crew = models.ForeignKey(CrewProfile, on_delete=models.CASCADE)
+    text = models.TextField(null=True, blank=True)
+    givenBy = models.CharField(max_length=256, null=True, blank=True)
+    
+class SocialLinks(models.Model):
+    crew = models.ForeignKey(CrewProfile, on_delete=models.CASCADE)
+    link = models.CharField(max_length=256, null=True, blank=True)
