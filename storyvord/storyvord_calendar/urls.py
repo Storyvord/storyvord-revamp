4bc0d1bc6ff9a37124092812facedc88df153540
Kaushik-Shahare <kaushikshahare0@gmail.com>
2024-08-02 18:39:59 +0530

    Updated storyvord-calendar

--- storyvord/storyvord_calendar/urls.py
+++ storyvord/storyvord_calendar/urls.py
@@ -3,7 +3,7 @@ from .views import CalendarView, EventView
 
 urlpatterns = [
     path('calendars/', CalendarView.as_view(), name='calendar-list'),
-    path('calendars/<int:pk>/', CalendarView.as_view(), name='calendar-detail'),
-    path('calendars/<int:calendar_id>/events/', EventView.as_view(), name='event-list-create'),
-    path('calendars/<int:calendar_id>/events/<int:pk>/', EventView.as_view(), name='event-detail'),
+    path('calendars/<str:project_id>/', CalendarView.as_view(), name='calendar-detail'),
+    path('calendars/<str:project_id>/events/', EventView.as_view(), name='event-list-create'),
+    path('calendars/<str:project_id>/events/<int:pk>/', EventView.as_view(), name='event-detail'),
 ]
