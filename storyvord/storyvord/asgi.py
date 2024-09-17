3dc5abae2be95d7b9d438985e0ea2883ec5a6977
rahberabbas <amaanabidi1234@gmail.com>
2024-09-17 23:15:15 +0530

    socket changes

--- storyvord/storyvord/asgi.py
+++ storyvord/storyvord/asgi.py
@@ -7,8 +7,38 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storyvord.settings')
+
+# django_asgi_app = get_asgi_application()
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from inbox import routing
+# from channels.security.websocket import AllowedHostsOriginValidator
+# import django
+# django.setup()
+# application = ProtocolTypeRouter(
+#     {
+#         "http": django_asgi_app,
+#         "websocket": AllowedHostsOriginValidator(
+#             AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+#         ),
+#     }
+# )
+
 import os
+
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storyvord.settings')
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import inbox.routing
@@ -16,11 +46,6 @@ import ai_assistant.routing
 from channels.security.websocket import AllowedHostsOriginValidator
 import django
 django.setup()
-
-from django.urls import path
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storyvord.settings')
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
