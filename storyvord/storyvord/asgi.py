56189c62babc5ec8464a2ccb9217757f5e41fe56
Shubhanshu Rawat <shubhanshu@alphamoney.in>
2024-09-18 13:58:21 +0530

    asgi changes

--- storyvord/storyvord/asgi.py
+++ storyvord/storyvord/asgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storyvord.settings')
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -21,7 +22,6 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 import django
 django.setup()
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
